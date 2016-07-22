e81569d71af79d3ba5534855c7a2b73327b34be5
lsx <lsx@163.com>
2016-07-22 15:51:51 +0800

    ActivityLife

---
+++ app/src/main/java/leo/activitylifetest/Activity2.java
@@ -0,0 +1,13 @@
+package leo.activitylifetest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class Activity2 extends AppCompatActivity{
+
+}
--- app/src/main/java/leo/activitylifetest/MainActivity.java
+++ app/src/main/java/leo/activitylifetest/MainActivity.java
@@ -1,8 +1,11 @@
 package leo.activitylifetest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity","onCreate is runned");
+        Button button = (Button) findViewById(R.id.activity_main_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                                          Intent intent = new Intent(MainActivity.this, Activity2.class);
+                                          startActivity(intent);
+                                      }
+                                  });
+
+
     }
 
     @Override
